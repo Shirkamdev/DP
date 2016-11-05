0a3d5d52a94dc36b36755428e570bb3a58d42190
shirkamdev <shirkamdev@gmail.com>
2016-11-05 08:12:44 +0100

    Started P100

--- Problemas/P100.java
+++ Problemas/P100.java
@@ -12,6 +12,45 @@ public class P100 {
 		solucion = new ArrayList<Integer>();
 	}
 
+	private void init(String v) {
+		System.out.println("----------");
+		System.out.println("-- Initial string:");
+		System.out.println("-- "+v);
+		System.out.println("----------");
+
+		String[] parts = v.split(" ");
+		N = Integer.parseInt(parts[0]);
+		L = Integer.parseInt(parts[1]);
+		for(int i=2; i<parts.length; i++) {
+			preferenciaDias.add(Integer.parseInt(parts[i]));
+		}
+
+		printVarValues();
+	}
+
+	public ArrayList<Integer> bestSolution(String s) {
+		init(s);
+		solucion = recursiveSolution(N, -1);
+
+		return solucion;
+	}
+
+	private ArrayList<Integer> recursiveSolution(int n, int last) {
+		if(n == 1) { //caso base
+			ArrayList<Integer> aux = new ArrayList<>();
+			aux.add(maxOfList(last));
+
+			//////
+			System.out.println("\tWith last="+last+" the best choice is "+aux.get(0)+" ValueOf: "+solutionListValue(aux));
+			/////
+
+			return aux;
+		}
+		else {
+			//TODO make this recursion great again XD
+		}
+	}
+
 	private void printVarValues() {
 		System.out.println("N: "+N);
 		System.out.println("L: "+L);
@@ -38,25 +77,38 @@ public class P100 {
 
 	}
 
-	private void init(String v) {
-		System.out.println("----------");
-		System.out.println("-- Initial string:");
-		System.out.println("-- "+v);
-		System.out.println("----------");
-
-		String[] parts = v.split(" ");
-		N = Integer.parseInt(parts[0]);
-		L = Integer.parseInt(parts[1]);
-		for(int i=2; i<parts.length; i++) {
-			preferenciaDias.add(Integer.parseInt(parts[i]));
+	private int maxOfList(int last) {
+		int max = 0;
+		for(int i=0; i<preferenciaDias.size(); ++i) {
+			if(Math.abs(last - i) >= L //If the distance between days is equal or greater than L
+				&& preferenciaDias.get(i) > preferenciaDias.get(max)) {
+				max = i;
+			}
 		}
 
-		printVarValues();
+		return max+1;
 	}
 
-	public ArrayList<Integer> bestSolution(String s) {
-		init(s);
+	private int solutionListValue(ArrayList<Integer> sol) {
+		int suma = 0;
+		for(int i=0; i<sol.size(); ++i) {
+			suma= preferenciaDias.get(sol.get(i)-1);
+		}
 
-		return solucion;
+		return suma;
 	}
+
+	private boolean isNotIntheList(ArrayList<Integer> list, int n) {
+		boolean found = false;
+
+		for(int i=0; i<list.size() && !found; ++i) {
+			if(list.get(i) == n) {
+				found = true;
+			}
+		}
+
+		return !found;
+	}
+
+
 }
--- Problemas/main.java
+++ Problemas/main.java
@@ -1,7 +1,22 @@
+import java.util.ArrayList;
+
 public class main {
 	public static void main(String[] args) {
 		P100 p = new P100();
+		ArrayList<Integer> solucion;
 
-		p.bestSolution("4 2 1 7 5 3 4");
+		for(int j=0; j<args.length; ++j) {
+			solucion = p.bestSolution(args[j]);
+
+			for(int i=0; i<solucion.size(); i++) {
+				System.out.print(solucion.get(i));
+				if(i != solucion.size()-1) {
+					System.out.print(", ");
+				}
+				else {
+					System.out.println();
+				}
+			}
+		}
 	}
 }
